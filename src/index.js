c369c0c7bf2da8d8800a1d14838887ae44eb91cf
Alex Robson <asrobson@gmail.com>
2015-07-27 02:09:32 -0500

    Support inherited IO

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
 var fsm = require( './fsm.js' );
-var process = require( './process.js' );
+var processMod = require( './process.js' );
 var set = require( './set.js' );
 var _ = require( 'lodash' );
 var os = require( 'os' );
 var hostPlatform = os.platform();
 
-function createStepper( platform, rawSteps, cwd ) {
-	if( !_.isString( platform ) ) {
-		cwd = rawSteps;
-		rawSteps = platform;
-		platform = hostPlatform;
-	}
+function createStepper( rawSteps, config ) {
+	config = config || {};
+	var platform = config.platform || os.platform;
+	var relativePath = config.relativePath || process.cwd();
+	var inherit = config.inheritIO || false;
+	var start = require( './process' ).start.bind( undefined, inherit );
 	var steps = set( platform, rawSteps );
-	return fsm( process.start, steps, cwd );
+	return fsm( start, steps, relativePath );
 }
 
 var fn = createStepper;
